25b3c184e9f7bbd0c7f6be2140fc8c5923d1fd07
천경훈 <37652329+ruNNiNg-V0V@users.noreply.github.com>
2023-05-10 23:37:21 +0900

    Update Max3.kt
    중앙값을 구하는 로직 추가
    입려된 수가 짝 수 일때, 홀 수 일때 조건식 추가

--- src/main/kotlin/chapter/one/Max3.kt
+++ src/main/kotlin/chapter/one/Max3.kt
@@ -22,27 +22,56 @@ class Max3 {
     }
 
     fun getMax() {
-        var a = 0
-        var b = 0
-        var c = 0
 
         println("세 정수의 최댓값을 구합니다")
 
         print("a의 값 : ")
-        a = setValue()
+        val a = setValue()
 
         print("b의 값 : ")
-        b = setValue()
+        val b = setValue()
 
         print("c의 값 : ")
-        c = setValue()
+        val c = setValue()
 
+        print("d의 값 : ")
+        val d = setValue()
+
+        print("e의 값 : ")
+        val e = setValue()
+
+        print("f의 값 : ")
+        val f = setValue()
+
+        print("g의 값 : ")
+        val g = setValue()
+
+        print("h의 값 : ")
+        val h = setValue()
+
+        /*
         var max = a
 
         if (b > max) max = b
         if (c > max) max = c
+         */
 
-        println("최대값은 ${max}입니다")
+        // 조건식을 사용하는게 아닌 정렬을 이용하여 최대값 출력
+        val list = listOf(a, b, c, d, e, f, g, h).sorted()
+
+        val size = list.size
+
+        val max = list.maxOf { it }
+
+        val mid = if (size % 2 == 0) {
+            // list가 짝수일 경우
+            list.subList(size / 2 - 1, size / 2 + 1)
+        } else {
+            // list가 홀수일 경우
+            list[size / 2]
+        }
+
+        println("최대값은 ${max}입니다, 중앙값은 ${mid}입니다.")
     }
 
     fun setValue(): Int = readln().toInt()
